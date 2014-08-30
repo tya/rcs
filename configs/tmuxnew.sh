f9827de1fed5bfcb8b82611a61fc2f58c26efd58
Hung Dang <hung.dang@sendgrid.com>
2014-08-29 23:00:01 -0600

    Split into four intead of two

--- configs/tmuxnew.sh
+++ configs/tmuxnew.sh
@@ -4,7 +4,10 @@ cd ~
 tmux new-session -d -s $1 'cd $PH && bash -i'
 tmux split-window -v
 #tmux send-keys -t ${env_name}:1.1 'vim $PT/todo.txt' Enter
-#tmux split-window -h
+tmux split-window -h
+tmux select-window -t 1
+tmux select-pane -t :1.0
+tmux split-window -h
 
 tmux new-window 'cd $PH && bash -i'
 #tmux send-keys -t ${env_name}:2.0 'vim' Enter
