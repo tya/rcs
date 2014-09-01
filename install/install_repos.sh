c2841931fec6f64761e12c3ba85b5bc548fa742a
Hung Dang <hung.dang@sendgrid.com>
2014-09-01 17:26:15 -0600

    Fix a minor bug in install_repos.sh

--- install/install_repos.sh
+++ install/install_repos.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 REP=~/sandbox/repos
+CURRENT=`pwd`
 cd $REP
 git clone https://github.com/seebi/dircolors-solarized.git
 git clone https://github.com/seebi/tmux-colors-solarized.git
 git clone https://github.com/altercation/solarized.git
 
 ln -sf $REP/dircolors-solarized/dircolors.256dark ~/.dir_colors
+cd $CURRENT
 
 
 
