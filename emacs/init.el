1607e2a5abab6e20dc3553629ed812aef96e4e83
Hung Dang <hung.dang@sendgrid.com>
2014-12-04 09:22:27 -0700

    Yes. I'm flirting with emacs

---
+++ emacs/init.el
@@ -0,0 +1,12 @@
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(require 'package)
+(package-initialize)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(require 'evil)
+(evil-mode 1)
