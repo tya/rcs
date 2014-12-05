b0a13becc69e608b76afa0238cb4abee487c3b9a
Hung Dang <hung.dang@sendgrid.com>
2014-12-05 16:06:11 -0700

    Work damn it

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path (concat user-emacs-directory "config"))
+(add-to-list 'load-path "~/rcs/emacs/config/")
 
 ;; packages
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
@@ -15,28 +15,9 @@
 
 (require 'use-package)
 
-;; don't need menu or tool bar
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-
-;; slime nav
-(require 'elisp-slime-nav)
-(defun my-lisp-hook ()
-  (elisp-slime-nav-mode)
-  (turn-on-eldoc-mode)
-  )
-(add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
-
-;; evil plugin
-(require 'evil)
-(evil-mode 1)
-(evil-define-key 'normal emacs-lisp-mode-map (kbd "K")
-  'elisp-slime-nav-describe-elisp-thing-at-point)
-(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-
+(require 'my-core)
+(require 'my-elisp)
+(require 'my-evil)
 
 
 
