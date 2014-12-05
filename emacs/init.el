6523f2e399a5be51b3d645586e7f0beeaa9210bc
Hung Dang <hung.dang@sendgrid.com>
2014-12-05 12:22:57 -0700

    Seperate out plugin setting

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,6 @@
+(add-to-list 'load-path (concat user-emacs-directory "config"))
+
+;; packages
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
@@ -5,8 +8,35 @@
 (require 'package)
 (package-initialize)
 
+(setq package-enable-at-startup nil)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;; don't need menu or tool bar
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
+;; slime nav
+(require 'elisp-slime-nav)
+(defun my-lisp-hook ()
+  (elisp-slime-nav-mode)
+  (turn-on-eldoc-mode)
+  )
+(add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
+
+;; evil plugin
 (require 'evil)
 (evil-mode 1)
+(evil-define-key 'normal emacs-lisp-mode-map (kbd "K")
+  'elisp-slime-nav-describe-elisp-thing-at-point)
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
+
+
+
