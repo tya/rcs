e7882eb0b9e9df85c5e3107ef4e780490448c0ae
emanlluf <gundamhung@yahoo.com>
2014-12-20 23:14:02 -0700

    Add rainbarf to repo needs to be monitored

--- install/install_repos.sh
+++ install/install_repos.sh
@@ -5,6 +5,7 @@ cd $REP
 git clone https://github.com/seebi/dircolors-solarized.git
 git clone https://github.com/seebi/tmux-colors-solarized.git
 git clone https://github.com/altercation/solarized.git
+git clone https://github.com/creaktive/rainbarf.git
 
 ln -sf $REP/dircolors-solarized/dircolors.256dark ~/.dir_colors
 cd $CURRENT
