e08bd5662a95d9d121c881f31837dd6073cb54e2
hvdang <hungvdang@gmail.com>
2015-04-26 08:14:36 -0700

    Dmenu vertical

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -6,7 +6,7 @@ application=$(
       # Show only app-names
       cut -d" " -f5 |\
       # Pipe to dmenu ($@ to include font settings from dwm/config.h)
-      dmenu -i -p "Switch to" -fn "-bitstream-*-*-*-*-*-16-*-*-*-*-*-*-*" $@
+      dmenu -i -l 10 -p "Switch to" -fn "-bitstream-*-*-*-*-*-16-*-*-*-*-*-*-*" $@
 )
 
 # Switch to chosen application
